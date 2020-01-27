8340083f1dfd591cb194c216875c15b00de10931
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-01-26 21:39:59 -0500

    Add project files.

---
+++ COMP212_Exc02_Predicate_Delegate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COMP212_Exc02_Predicate_Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
